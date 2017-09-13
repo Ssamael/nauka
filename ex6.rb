a53c160e9a10392da432d5dcc9523555c5227140
Ssamael <hikokoro06@gmail.com>
2017-09-13 20:19:06 +0200

    exercise 6

---
+++ ex6.rb
@@ -0,0 +1,21 @@
+types_of_people = 10 # 10 w kodzie binarnym oznacza 2
+x = "There are #{types_of_people} types of people." # w sumie to wydaje mi sie
+binary = "binary" # ze to polecenie pisania komentarzy
+do_not = "don't" # ktore jest przy prawie kazdym cwiczeniu
+y = "Those who know #{binary} and those who #{do_not}." # to ma tylko na celu
+
+puts x # wyrobienie nawyku
+puts y # opisywania kazdej linijki kodu
+
+puts "I said #{x}." # komentarzmi, ktore
+puts "I also said '#{y}'." # teoretycznie, pozniej powinny
+
+hilarious = false # ulatwic odnalezienie sie w kodzie
+joke_evaluation = "Isn't that joke so funny? #{hilarious}" # aczkolwiek na chwile obecna
+
+puts joke_evaluation # jest to dosc nudne
+
+w = "This is the left side of..." # moze dlatego, ze przy tak krotkim
+e = "a string with a right side." # i prostym kodzie
+
+puts w + e # to ciezko czegos nie ogarnac albo sie zgubic
