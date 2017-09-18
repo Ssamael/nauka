5017a646fc05f971d59900eb2f86d14a70ce19dd
Ssamael <hikokoro06@gmail.com>
2017-09-18 13:54:13 +0200

    ble bleble

--- ex10.rb
+++ ex10.rb
@@ -7,10 +7,12 @@ I'll do a list:
 \t* Cat food
 \t* Fishies
 \t* Catnip\n\t* Grass
-"""
+""" 
+# If u use ''' then \t will not work good
 
 
 puts tabby_cat
 puts persian_cat
 puts backslash_cat
 puts fat_cat
+
