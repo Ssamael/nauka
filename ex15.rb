9038ed36c0bc2c845c247a2d29fbd012099809c8
Ssamael <hikokoro06@gmail.com>
2017-10-01 21:07:03 +0200

    excercise 15 with text file

---
+++ ex15.rb
@@ -0,0 +1,17 @@
+filename = ARGV.first #command line argument
+
+txt = open(filename) # now that file is an object
+
+puts "Here's your file #{filename}:" # some information to user
+print txt.read # reading the file
+
+txt.close
+
+print "Type the filename again: "
+file_again = $stdin.gets.chomp # asking user about file name
+
+txt_again = open(file_again) # transform file into an object
+
+print txt_again.read # reading and printing the file
+
+txt_again.close 
